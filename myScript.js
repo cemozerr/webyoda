35e005612fdd31a6c4664d6d251d93ad020ee453
cemozerr <cemozer2018@u.northwestern.edu>
2016-08-19 22:17:08 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,1 +1,17 @@
-
+src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
+    function doIt() {
+      string = $("#test").val()
+      if (string == ''){
+        alert("YOU CANT JUST SHOUT NOTHING, YOU COWARD");
+        return;
+      }
+      var replaced = string.replace(/ /g, '+');
+      var output = $.ajax({
+        url: "https://yoda.p.mashape.com/yoda?sentence=" + replaced, // The URL to the API. 
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        dataType: 'text',
+        success: function(data) {document.getElementById("output").innerHTML = data; },
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader("X-Mashape-Authorization", "yUyYx3ozJVmshXYVYcfln7FYGOHHp1wzb7xjsnopzf0dvjkBzE");
+          xhr.setRequestHeader("Accept", "text/plain");// Enter here your Mashape key
+          }});}
