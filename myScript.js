fdc979c60dae6332936c78f1478ad516557f803d
cemozerr <cemozer2018@u.northwestern.edu>
2016-08-19 23:09:40 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,19 +1,19 @@
  function doIt() {
-      string = $("#test").val()
-      if (string == ''){
-        alert("YOU CANT JUST SHOUT NOTHING, YOU COWARD");
-        return;
-      }
-      var replaced = string.replace(/ /g, '+');
-      var output = $.ajax({
-        url: "https://yoda.p.mashape.com/yoda?sentence=" + replaced, // The URL to the API. 
-        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-        dataType: 'text',
-        success: function(data) {document.getElementById("output").innerHTML = data; },
-        beforeSend: function(xhr) {
-          xhr.setRequestHeader("X-Mashape-Authorization", "yUyYx3ozJVmshXYVYcfln7FYGOHHp1wzb7xjsnopzf0dvjkBzE");
-          xhr.setRequestHeader("Accept", "text/plain");// Enter here your Mashape key
-          }});}
+   string = $("#test").val()
+   if (string == ''){
+     alert("YOU CANT JUST SHOUT NOTHING, YOU COWARD");
+     return;
+   }
+   var replaced = string.replace(/ /g, '+');
+   var output = $.ajax({
+     url: "https://yoda.p.mashape.com/yoda?sentence=" + replaced, // The URL to the API. 
+     type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+     dataType: 'text',
+     success: function(data) {document.getElementById("output").innerHTML = data; },
+     beforeSend: function(xhr) {
+       xhr.setRequestHeader("X-Mashape-Authorization", "yUyYx3ozJVmshXYVYcfln7FYGOHHp1wzb7xjsnopzf0dvjkBzE");
+       xhr.setRequestHeader("Accept", "text/plain");// Enter here your Mashape key
+       }});}
 $(".button").click(function(){
- doIt();
+  doIt();
 };
